c16e3118a2eebaadd5744d7dde43c0feffedffa6
mmelvin <33356311+mmelvin@users.noreply.github.com>
2024-10-07 22:08:36 +0000

    initial

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,22 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+all_selections = {
+    'Lufkin': [
+        '# of meters',   
+    ],
+    'TotalFlow': [
+        '# of meters',   
+    ],
+    'AutoPilot': [
+        '# of meters',   
+    ]
+}
+
+option = st.selectbox(
+    "Device type?",
+    all_selections.keys()
+)
+
+st.write("You selected:", option)
